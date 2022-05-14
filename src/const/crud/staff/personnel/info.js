44238d2ecee8b39d800d6ac645cfee2a1c7cd3c9
3010086711@qq.com <3010086711@qq.com>
2022-05-14 21:59:13 +0800

    update 人员分类

--- src/const/crud/staff/personnel/info.js
+++ src/const/crud/staff/personnel/info.js
@@ -1,1 +1,73 @@
-
+export const option = {
+  editBtn: false,
+  addBtn: false,
+  border: true,
+  searchSpan: 7,
+  searchMenuSpan: 4,
+  menuWidth: 80,
+  column: [{
+      label: "姓名",
+      prop: "xm",
+      search: true,
+      slotForm: true,
+    },
+    {
+      label: "工号",
+      prop: "gh",
+      search: true,
+      width: 120,
+    },
+    {
+      label: "部门",
+      prop: "orgId",
+      search: true,
+      type: 'tree',
+      dicUrl: 'admin/dept/tree',
+      props: {
+        label: "name",
+        value: "id",
+        children: "children"
+      },
+      width: 120,
+    },
+    {
+      label: "编制类型",
+      prop: "bzlx",
+      search: true,
+      type: 'select',
+      dicUrl: '/admin/dict/type/bz_type',
+      props: {
+        label: "label",
+        value: "value",
+      },
+    },
+    {
+      label: "人员类型",
+      prop: "rylx",
+      search: true,
+      searchSpan: 7,
+      type: 'select',
+      dicUrl: '/admin/dict/type/rylb_type',
+      props: {
+        label: "label",
+        value: "value",
+      },
+    },
+    {
+      label: "教师类型",
+      prop: "jslx",
+      search: true,
+      searchSpan: 6,
+      type: 'select',
+      dicUrl: '/admin/dict/type/teacher_type',
+      props: {
+        label: "label",
+        value: "value",
+      },
+    },
+    {
+      label: "入职日期",
+      prop: "rzrq",
+    },
+  ],
+}
--- src/views/staff/personnel/info.vue
+++ src/views/staff/personnel/info.vue
@@ -1,185 +1,39 @@
 <template>
   <div class="engage_container">
     <basic-container>
-      <avue-crud
-        :option="option"
-        :search.sync="search"
-        :data="data"
-        :page.sync="page"
-      >
-        <template slot="orgIdSearch">
-          <avue-input-tree
-            v-model="search.orgId"
-            :dic="treeDeptData"
-            :props="defaultProps"
-            placeholder="请选择所属部门"
-          />
-        </template>
-        <template slot="bzlxSearch">
-          <el-select v-model="bzValue" placeholder="请选择">
-            <el-option
-              v-for="item in bzlxList"
-              :key="item.id"
-              :label="item.label"
-              :value="item.id"
-            >
-            </el-option>
-          </el-select>
-        </template>
-        <template slot="rylxSearch">
-          <el-select v-model="ryValue" placeholder="请选择">
-            <el-option
-              v-for="item in ryList"
-              :key="item.value"
-              :label="item.name"
-              :value="item.id"
-            >
-            </el-option>
-          </el-select>
-        </template>
-        <template slot="jslxSearch">
-          <el-select v-model="jsValue" placeholder="请选择">
-            <el-option
-              v-for="item in jslxList"
-              :key="item.id"
-              :label="item.label"
-              :value="item.id"
-            >
-            </el-option>
-          </el-select>
-        </template>
-      </avue-crud>
+      <avue-crud :option="option" :data="data" :page.sync="page"> </avue-crud>
     </basic-container>
   </div>
 </template>
 
 <script>
-import { getDept, getJslx, getBzlx } from "@/const/staff/getSelectOption";
+import { option } from "@/const/crud/staff/personnel/info";
 
 export default {
   name: "TableEngage",
   data() {
     return {
-      treeDeptData: undefined,
-      defaultProps: {
-        label: "name",
-        value: "id",
-      },
-      page: {
-        total: 1000,
-        currentPage: 1,
-        pageSize: 10,
-      },
       // 数据源
       data: [
         {
           gh: "10086",
           xm: "张三",
-          objId: "未知",
+          orgId: 1,
           bzlx: "事业编制",
           rylx: "在职人员",
           jslx: "外聘教师",
-          rzrq: "2022/02/02",
+          rzrq: "2022-02-02",
         },
       ],
-      option: {
-        editBtn: false,
-        addBtn: false,
-        border: true,
-        searchMenuSpan: 4,
-        column: [
-          {
-            label: "工号",
-            prop: "gh",
-            search: true,
-            searchSpan: 7,
-          },
-          {
-            label: "姓名",
-            prop: "xm",
-            search: true,
-            searchSpan: 7,
-          },
-          {
-            label: "所属部门",
-            prop: "orgId",
-            search: true,
-            searchSpan: 7,
-            searchslot: true,
-          },
-          {
-            label: "编制类型",
-            prop: "bzlx",
-            search: true,
-            searchSpan: 7,
-            searchslot: true,
-          },
-          {
-            label: "人员类型",
-            prop: "rylx",
-            search: true,
-            searchSpan: 7,
-            searchslot: true,
-          },
-          {
-            label: "教师类型",
-            prop: "jslx",
-            search: true,
-            searchSpan: 6,
-            searchslot: true,
-          },
-          {
-            label: "入职日期",
-            prop: "rzrq",
-          },
-        ],
+      option: option,
+      page: {
+        total: 1000,
+        currentPage: 1,
+        pageSize: 10,
       },
-      // 搜索的表单对象
-      search: {},
-      depList: [
-        { id: 1, name: "部门1" },
-        { id: 2, name: "部门2" },
-        { id: 3, name: "部门3" },
-        { id: 4, name: "部门4" },
-        { id: 5, name: "部门5" },
-        { id: 6, name: "部门6" },
-        { id: 7, name: "部门7" },
-      ],
-      depValue: "",
-      bzlxList: undefined,
-      bzValue: "",
-      ryList: [
-        { id: 1, name: "在职人员" },
-        { id: 2, name: "离校人员" },
-        { id: 3, name: "退休人员" },
-      ],
-      ryValue: "",
-      jslxList: undefined,
-      jsValue: "",
     };
   },
-  methods: {
-    async getDept() {
-      const { data: res } = await getDept();
-      if (res.code !== 0) return this.$message.error(res.msg);
-      this.treeDeptData = res.data;
-    },
-    async getJslx() {
-      const { data: res } = await getJslx();
-      if (res.code !== 0) return this.$message.error(res.msg);
-      this.jslxList = res.data;
-    },
-    async getBzlx() {
-      const { data: res } = await getBzlx();
-      if (res.code !== 0) return this.$message.error(res.msg);
-      this.bzlxList = res.data;
-    },
-  },
-  mounted() {
-    this.getDept();
-    this.getJslx();
-    this.getBzlx();
-  },
+  methods: {},
 };
 </script>
 
